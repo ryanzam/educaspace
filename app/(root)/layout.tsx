8a1e8449045dcd92db9d7cd02a14cc5ef7b9b1ef
ranjam <ryanzam2005@gmail.com>
2025-01-07 14:40:01 +0545

    project setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default HomeLayout
